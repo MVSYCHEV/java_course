c772189905f84241652327e1ca2f61929cd3fbf1
MVSYCHEV <mihailcook@gmail.com>
2021-07-19 20:03:18 +0300

    Lection2.5 add test DeleteGroupTest and rename some methods
    Поправил название методов before/after в ранее созданном классе ContactCreationTest, и переименовал сам метод

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/ContactTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/ContactTests.java
@@ -10,7 +10,7 @@ import org.openqa.selenium.*;
 public class ContactTests {
   private WebDriver wd;
 
-  @BeforeClass(alwaysRun = true)
+  @BeforeMethod(alwaysRun = true)
   public void setUp() throws Exception {
     wd = new ChromeDriver();
     wd.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
@@ -29,7 +29,7 @@ public class ContactTests {
   }
 
   @Test
-  public void testContactTests() throws Exception {
+  public void testContactCreation() throws Exception {
     initContactCreation();
     fillContactForm(new ContactData("Mikhail", "Sychev", "Testovich", "RogaAndCopyta", "OOO", "Pushkin street", "+77777777777", "test@testovich.google.org"));
     submitContactCreation();
@@ -80,7 +80,7 @@ public class ContactTests {
     wd.findElement(By.linkText("add new")).click();
   }
 
-  @AfterClass(alwaysRun = true)
+  @AfterMethod(alwaysRun = true)
   public void tearDown() throws Exception {
     wd.quit();
   }
