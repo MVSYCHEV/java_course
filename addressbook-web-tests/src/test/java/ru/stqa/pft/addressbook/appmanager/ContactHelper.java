285342e09836de14094259de41a07fc307fc0cc3
MVSYCHEV <mihailcook@gmail.com>
2021-07-20 10:30:09 +0300

    Task5 create class ContactHelper and refactoring the test

---
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ContactHelper.java
@@ -0,0 +1,36 @@
+package ru.stqa.pft.addressbook.appmanager;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import ru.stqa.pft.addressbook.model.ContactData;
+
+public class ContactHelper extends BaseHelper{
+
+  public ContactHelper(WebDriver wd) {
+    super(wd);
+  }
+
+  public void initContactCreation() {
+    click(By.linkText("add new"));
+  }
+
+  public void returnToContactPage() {
+    click(By.linkText("home page"));
+  }
+
+  public void submitContactCreation() {
+    click(By.name("submit"));
+  }
+
+  public void fillContactForm(ContactData contactData) {
+    type(By.name("firstname"), contactData.getName());
+    type(By.name("lastname"), contactData.getLastname());
+    type(By.name("nickname"), contactData.getNickname());
+    type(By.name("title"), contactData.getTitle());
+    type(By.name("company"), contactData.getCompany());
+    type(By.name("address"), contactData.getAddress());
+    type(By.name("mobile"), contactData.getPhone());
+    type(By.name("email"), contactData.getEmail());
+  }
+
+}
