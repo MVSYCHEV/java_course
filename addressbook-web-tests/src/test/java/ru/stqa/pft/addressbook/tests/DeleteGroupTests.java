adc369a2cfe66b186c303f04c6f85229e81544c9
MVSYCHEV <mihailcook@gmail.com>
2021-07-24 14:46:32 +0300

    Lection3.10 add and check precondition in tests for groups

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/DeleteGroupTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/DeleteGroupTests.java
@@ -2,12 +2,16 @@ package ru.stqa.pft.addressbook.tests;
 
 
 import org.testng.annotations.Test;
+import ru.stqa.pft.addressbook.model.GroupData;
 
 public class DeleteGroupTests extends TestBase {
 
   @Test
-  public void testDeleteGroup() throws Exception {
+  public void testDeleteGroup() {
     app.getNavigationHelper().goToGroupPage();
+    if (!app.getGroupHelper().isThereIsGroup()) {
+      app.getGroupHelper().createGroup(new GroupData("test123", null, null));
+    }
     app.getGroupHelper().selectGroup();
     app.getGroupHelper().deleteSelectedGroups();
     app.getGroupHelper().returnToGroupPage();
