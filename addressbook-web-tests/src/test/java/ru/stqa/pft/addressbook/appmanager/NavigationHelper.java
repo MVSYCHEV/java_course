35263d49623946614720d9165eb6227293ca7d1e
MVSYCHEV <mihailcook@gmail.com>
2021-07-24 14:33:45 +0300

    Lection3.9 optimize transitions between pages

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/NavigationHelper.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/NavigationHelper.java
@@ -12,6 +12,11 @@ public class NavigationHelper extends BaseHelper {
   }
 
   public void goToGroupPage() {
+    if (isElementPresent(By.tagName("h1"))
+            && wd.findElement(By.tagName("h1")).getText().equals("Groups")
+            && isElementPresent(By.name("new"))) {
+      return;
+    }
     click(By.linkText("groups"));
   }
 
@@ -21,6 +26,11 @@ public class NavigationHelper extends BaseHelper {
   }
 
   public void goToContactPage() {
+    if (isElementPresent(By.id("maintable"))) {
+      return;
+    }
     click(By.linkText("home"));
   }
+
+
 }
