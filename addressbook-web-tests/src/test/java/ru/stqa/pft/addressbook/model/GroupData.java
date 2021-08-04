e326fb1e07ba42d45ff2a5719de996746dd037a2
MVSYCHEV <mihailcook@gmail.com>
2021-08-04 19:07:45 +0300

    Lection4.7 add hashSet into tests

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/GroupData.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/GroupData.java
@@ -4,19 +4,33 @@ import java.util.Objects;
 
 public class GroupData {
   private final String name;
+  private final String id;
   private final String header;
   private final String footer;
 
-  public GroupData(String name, String header, String footer) {
+  public GroupData(String name, String id, String header, String footer) {
     this.name = name;
+    this.id = id;
     this.header = header;
     this.footer = footer;
+  }
+
+  public GroupData(String name, String header, String footer) {
+    this.name = name;
+    this.id = null;
+    this.header = header;
+    this.footer = footer;
+  }
+
+  public String getId() {
+    return id;
   }
 
   @Override
   public String toString() {
     return "GroupData{" +
             "name='" + name + '\'' +
+            ", id='" + id + '\'' +
             '}';
   }
 
@@ -25,12 +39,13 @@ public class GroupData {
     if (this == o) return true;
     if (o == null || getClass() != o.getClass()) return false;
     GroupData groupData = (GroupData) o;
-    return Objects.equals(name, groupData.name);
+    return Objects.equals(name, groupData.name) &&
+            Objects.equals(id, groupData.id);
   }
 
   @Override
   public int hashCode() {
-    return Objects.hash(name);
+    return Objects.hash(name, id);
   }
 
   public String getName() {
--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupModificationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupModificationTests.java
@@ -4,9 +4,10 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.GroupData;
 
+import java.util.HashSet;
 import java.util.List;
 
-public class GroupModificationTests extends TestBase{
+public class GroupModificationTests extends TestBase {
 
   @Test
   public void testGroupModification() {
@@ -17,10 +18,15 @@ public class GroupModificationTests extends TestBase{
     List<GroupData> before = app.getGroupHelper().getGroupList();
     app.getGroupHelper().selectGroup(before.size() - 1);
     app.getGroupHelper().initGroupModification();
-    app.getGroupHelper().fillGroupForm(new GroupData("test123", "test123", "test123"));
+    GroupData group = new GroupData("test123", before.get(before.size() - 1).getId(),"test123", "test123");
+    app.getGroupHelper().fillGroupForm(group);
     app.getGroupHelper().submitGroupModification();
     app.getGroupHelper().returnToGroupPage();
     List<GroupData> after = app.getGroupHelper().getGroupList();
     Assert.assertEquals(after.size(), before.size());
+
+    before.remove(before.size() - 1);
+    before.add(group);
+    Assert.assertEquals(new HashSet<Object>(before), new HashSet<Object>(after));
   }
 }
