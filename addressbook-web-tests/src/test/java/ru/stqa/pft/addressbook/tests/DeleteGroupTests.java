ff9c97c98a827528eb783d63ed21f1bfe4e4768e
MVSYCHEV <mihailcook@gmail.com>
2021-08-04 18:10:33 +0300

    Lection4.3 check collections elements

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/DeleteGroupTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/DeleteGroupTests.java
@@ -1,6 +1,7 @@
 package ru.stqa.pft.addressbook.tests;
 
 
+import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.GroupData;
 
@@ -9,12 +10,15 @@ public class DeleteGroupTests extends TestBase {
   @Test
   public void testDeleteGroup() {
     app.getNavigationHelper().goToGroupPage();
+    int before = app.getGroupHelper().getGroupCount();
     if (!app.getGroupHelper().isThereIsGroup()) {
       app.getGroupHelper().createGroup(new GroupData("test123", null, null));
     }
     app.getGroupHelper().selectGroup();
     app.getGroupHelper().deleteSelectedGroups();
     app.getGroupHelper().returnToGroupPage();
+    int after = app.getGroupHelper().getGroupCount();
+    Assert.assertEquals(after, before - 1);
     app.logout();
   }
 }
