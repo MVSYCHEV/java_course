3b44e33cec0a9a1b45c6e925c7164f663612bb35
MVSYCHEV <mihailcook@gmail.com>
2021-09-03 11:34:58 +0300

    Lection8.2 add neew project for Mantis

--- mantis-tests/src/test/java/ru/stqa/pft/mantis/appmanager/ApplicationManager.java
+++ mantis-tests/src/test/java/ru/stqa/pft/mantis/appmanager/ApplicationManager.java
@@ -1,6 +1,5 @@
-package ru.stqa.pft.addressbook.appmanager;
+package ru.stqa.pft.mantis.appmanager;
 
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
@@ -16,23 +15,17 @@ public class ApplicationManager {
   private final Properties properties;
   WebDriver wd;
 
-  private NavigationHelper navigationHelper;
-  private GroupHelper groupHelper;
-  private SessionHelper sessionHelper;
-  private ContactHelper contactHelper;
   private String browser;
-  private DbHelper dbHelper;
 
-  public ApplicationManager(String browser) {
+  public ApplicationManager(String browser){
     this.browser = browser;
     properties = new Properties();
   }
 
   public void init() throws IOException {
     String target = System.getProperty("target", "local");
-    properties.load(new FileReader(new File(String.format("src/test/resources/%s.properties", target))));
+    properties.load(new FileReader(new File(String.format("src/test/resources/%s.properties",target))));
 
-    dbHelper = new DbHelper();
 
     if (browser.equals(BrowserType.CHROME)) {
       wd = new ChromeDriver();
@@ -44,34 +37,9 @@ public class ApplicationManager {
 
 //    wd.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
     wd.get(properties.getProperty("web.baseUrl"));
-    groupHelper = new GroupHelper(wd);
-    contactHelper = new ContactHelper(wd);
-    navigationHelper = new NavigationHelper(wd);
-    sessionHelper = new SessionHelper(wd);
-    sessionHelper.login(properties.getProperty("web.adminLogin"), properties.getProperty("web.adminPassword"));
-  }
 
-  public void logout() {
-    wd.findElement(By.linkText("Logout")).click();
   }
-
   public void stop() {
     wd.quit();
-  }
-
-  public GroupHelper group() {
-    return groupHelper;
-  }
-
-  public NavigationHelper goTo() {
-    return navigationHelper;
-  }
-
-  public ContactHelper contact() {
-    return contactHelper;
-  }
-
-  public DbHelper db() {
-    return dbHelper;
   }
 }
