991b8429d091d0d511eac02481f367d1f996094f
MVSYCHEV <mihailcook@gmail.com>
2021-09-12 12:29:03 +0300

    Lection9.6 REST interaction with GitHub

---
+++ github-sample/src/test/java/ru/stqa/pft/github/GitHubTests.java
@@ -0,0 +1,19 @@
+package ru.stqa.pft.github;
+
+import com.google.common.collect.ImmutableMap;
+import com.jcabi.github.*;
+import org.testng.annotations.Test;
+
+import java.io.IOException;
+
+public class GitHubTests {
+
+  @Test
+  public void testCommits() throws IOException {
+    Github github = new RtGithub("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");
+    RepoCommits commits = github.repos().get(new Coordinates.Simple("mvsychev", "java_course")).commits();
+    for (RepoCommit commit : commits.iterate(new ImmutableMap.Builder<String, String>().build())) {
+      System.out.println(new RepoCommit.Smart(commit).message());
+    }
+  }
+}
