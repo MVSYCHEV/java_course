ecb7c78ae15db529ba85fb5a3fa82e2111429f3f
MVSYCHEV <mihailcook@gmail.com>
2021-09-13 21:41:36 +0300

    Task20 review fix

--- rest-sample/src/test/java/ru/stqa/pft/rest/Tests/TestBase.java
+++ rest-sample/src/test/java/ru/stqa/pft/rest/Tests/TestBase.java
@@ -1,9 +1,14 @@
 package ru.stqa.pft.rest.Tests;
 
+import com.google.gson.Gson;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonParser;
+import com.google.gson.reflect.TypeToken;
+import org.apache.http.client.fluent.Executor;
+import org.apache.http.client.fluent.Request;
 import org.testng.SkipException;
 
 import java.io.IOException;
-import java.util.Objects;
 import java.util.Set;
 
 public class TestBase {
@@ -14,9 +19,18 @@ public class TestBase {
     }
   }
 
+  public static Executor getExecutor() {
+    return Executor.newInstance().auth("288f44776e7bec4bf44fdfeb1e646490", "");
+  }
+
   private boolean isIssueOpen(int issueId) throws IOException {
-    Set<Issue> issues = RestHelper.getIssues();
-    Issue issue = issues.stream().filter(Issue -> Objects.equals(issueId, Issue.getId())).findFirst().get();
-    return !issue.getStateName().equals("Closed");
+    String json = getExecutor().execute(Request.Get("https://bugify.stqa.ru/api/issues/" + issueId + ".json")).returnContent().asString();
+    JsonElement parsed = new JsonParser().parse(json);
+    JsonElement issues = parsed.getAsJsonObject().get("issues");
+    Set<Issue> issue = new Gson().fromJson(issues, new TypeToken<Set<Issue>>() {
+    }.getType());
+    Issue myIssue = issue.iterator().next();
+    String stateName = myIssue.getStateName();
+    return !stateName.equals("Closed");
   }
 }
